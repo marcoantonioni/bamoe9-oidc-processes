b3a374ca5212429f56777729feff7cdba7780de2
marcoantonioni <antonioni.marco@gmail.com>
2024-12-17 15:35:08 +0100

    updates

--- jbpm-compact-architecture-security/target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ jbpm-compact-architecture-security/target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "938f73f0-6c8e-49f0-bed5-e13260ba226a";
+private static final String ID = "60fd7ada-66e2-4b63-be0c-f2bccdec74d3";
 }
