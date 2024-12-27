b3edd996e120b0092b014a9d4021a056d1e0fc6a
marcoantonioni <antonioni.marco@gmail.com>
2024-12-27 11:58:55 +0100

    updates

--- jbpm-compact-architecture-security/generate-env-vars.sh
+++ jbpm-compact-architecture-security/generate-env-vars.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 PROPERTIES_FILE=./src/main/resources/application.properties
 if [[ -f ${PROPERTIES_FILE} ]]; then
-  cat ${PROPERTIES_FILE} | sed 's/=.*/=/g' | sed 's/^%.*//g'| sed 's/^#.*//g' | sed '/^$/d' | sed 's/-/_/g' | sed 's/\./_/g' | sed 's/[a-z]/\U&/g'
+  cat ${PROPERTIES_FILE} \
+    | sed 's/=.*//g' \
+    | sed 's/^%.*//g' \
+    | sed 's/^#.*//g' \
+    | sed '/^$/d' \
+    | sed 's/-/_/g' \
+    | sed 's/\//_/g' \
+    | sed 's/\./_/g' \
+    | sed 's/[a-z]/\U&/g' \
+    | sed 's/^/            - name: /g' \
+    | sort
 fi
-
